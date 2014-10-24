b91712ad7d3b40eb77d7be793715e6c6f3e3298f
Jonathan Shih <joncshih@gmail.com>
2014-10-23 22:31:47 -0700

    generation, some integration test

--- lib/generation.rb
+++ lib/generation.rb
@@ -1,9 +1,13 @@
 require 'set'
+require 'rules'
 
 class Generation
-  def initialize(cells, rules)
+  include Rules
+
+  attr_reader :live_cells
+
+  def initialize(cells)
     @live_cells = Set.new(cells)
-    @rules = rules
   end
 
   def population
@@ -11,6 +15,32 @@ class Generation
   end
 
   def tick
-    Generation.new([], @rules)
+    Generation.new(live_cells_next)
+  end
+
+  def live_cells_next
+    cells_stay_alive + cells_come_alive
+  end
+
+  def cells_stay_alive
+    candidate_live_cells.find_all do |c|
+      stays_alive?(c.neighbors & candidate_live_cells)
+    end.to_set
+  end
+
+  def cells_come_alive
+    candidate_dead_cells.find_all do |c|
+      comes_alive?(c.neighbors & candidate_live_cells)
+    end.to_set
+  end
+
+  def candidate_live_cells
+    @live_cells
+  end
+
+  def candidate_dead_cells
+    @live_cells.flat_map do |c|
+      c.neighbors
+    end.reduce(:+) - @live_cells
   end
 end
--- spec/lib/generation_spec.rb
+++ spec/lib/generation_spec.rb
@@ -1,25 +1,34 @@
 require 'generation'
+require 'cell'
 
 describe Generation do
-  let(:rules) { double("rules") }
-
-  context 'empty' do
-    let(:gen) { Generation.new([], rules) }
-
-    it 'calls .stays_alive? 0 times' do
-      allow(rules).to receive(:stays_alive?) { rules }
-      gen.tick
-      expect(rules).to receive(:stays_alive?).exactly(0).times
-    end
-
-    it 'calls .comes_alive? 0 times' do
-      allow(rules).to receive(:comes_alive?) { rules }
-      gen.tick
-      expect(rules).to receive(:comes_alive?).exactly(0).times
+  describe "#candidate_dead_cells" do
+    it "gets correct candidates for overlapping neighbors" do
+      gen = Generation.new([Cell.new(0, 0), Cell.new(1, 1)])
+      expect(gen.candidate_dead_cells.length).to eq(12)
     end
   end
 
-  context '1 cell'
+  describe "#cells_stay_alive" do
+    it "returns correct survivor" do
+      gen = Generation.new([Cell.new(3, 3), Cell.new(3, 4), Cell.new(4, 5)])
+      expect(gen.cells_stay_alive).to eq(Set.new([Cell.new(3, 4)]))
+    end
+  end
 
-  context 'overlapping neighbor space'
+  describe "#cells_come_alive" do
+    it "returns correct birth" do
+      gen = Generation.new([Cell.new(6, 3), Cell.new(7, 4), Cell.new(6, 5)])
+      expect(gen.cells_come_alive).to eq(Set.new([Cell.new(6, 4)]))
+    end
+  end
+end
+
+describe "still life" do
+end
+
+describe "oscillator" do
+end
+
+describe "glider" do
 end
